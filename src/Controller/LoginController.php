e5eee9b0f438424320aa9ab6c949168e6559cb82
Paulina <paulinarygula5@gmail.com>
2025-04-06 18:42:40 +0200

    Initial commit

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+	public function logout(){}
+
+}
